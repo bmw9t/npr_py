cd60514b8fbef31abf1f2498931843f8d3aa380e
Brandon Walsh <walshb@wlu.edu>
2016-06-10 16:03:47 -0400

    has some frequency distribution supports. the api seems robust, but there seems to be a fair amount of differences among the different api results. sometimes "text" is the thing that tells you if there is text. sometimes it is "fulltext." and it seems like at a certain scale it collapses the results into further api queries for you to call.

--- npr_py.py
+++ npr_py.py
@@ -1,18 +1,26 @@
 import requests
+from nltk import word_tokenize, FreqDist
+from nltk.corpus import stopwords
 
 URL = 'http://api.npr.org/query?'
 
 PARAM = {
     'apiKey': 'MDI0NjkyODAwMDE0NjU1NzEzNTBjNzhkNw000',
-    'id': '3002',
+    'id': '1105',
     'output': 'JSON',
-    'fields': 'teaser'
+    'fields': 'text'
 }
+
+# Functions for interacting with the API
 
 
 def get_results(url=URL, params=PARAM):
     res = requests.get(url, params=PARAM)
-    return res.json()['list']['story']
+    try:
+        return res.json()['list']['story']
+    except:
+        print('problem with api query. none returned')
+        print(res.url)
 
 
 def add_or_update_params(key, value):
@@ -25,17 +33,48 @@ def delete_params(key):
     del(PARAM[key])
 
 
+# Analysis functions
+
+
+def get_text(results):
+    """takes some API results and returns a big
+    string of all the text in it."""
+    api_text_results = []
+    for result in results:
+        try:
+            paragraphs = result['text']['paragraph']
+        except:
+            print(result)
+        for paragraph in paragraphs:
+            try:
+                if '$text' in paragraph:
+                    api_text_results.append(paragraph['$text'])
+            except:
+                print('==========')
+                print(paragraph)
+                print(paragraph['num'])
+    return ' '.join(api_text_results)
+
+
+def tokenize(text):
+    """take the test lump and tokenize it."""
+    punctuation = [',', '-', '.', '!', "\"", '\'' ':', ';', '?', '...',
+                   '\'\'', '``', ']', '[', '—', ':']
+    stopwords_list = stopwords.words('english') + punctuation
+    return list(w.lower() for w in word_tokenize(text) if
+                w.lower() not in stopwords_list)
+
+
+def the_whole_thing():
+    res = get_results()
+    text = get_text(res)
+    fd = FreqDist(tokenize(text))
+    return fd.most_common(50)
+
+
 def main():
     res = get_results()
-    for result in res:
-        print('======')
-        print(result)
-
-    print(PARAM)
-    add_or_update_params('test', 'a thing')
-    print(PARAM)
-    delete_params('test')
-    print(PARAM)
+    print(the_whole_thing())
 
 
 if __name__ == '__main__':
